ec0008753b7726e8c5db06d48faf5856f36d18da
inrgihc <inrgihc@126.com>
2023-12-16 20:59:04 +0800

    修正代码错误

--- dbswitch-product/dbswitch-product-mongodb/src/main/java/com/gitee/dbswitch/product/mongodb/MongodbTableDataWriteProvider.java
+++ dbswitch-product/dbswitch-product-mongodb/src/main/java/com/gitee/dbswitch/product/mongodb/MongodbTableDataWriteProvider.java
@@ -73,11 +73,7 @@ public class MongodbTableDataWriteProvider extends DefaultTableDataWriteProvider
     for (Object[] row : recordValues) {
       Map<String, Object> columns = new LinkedHashMap<>(fieldCount);
       for (int i = 0; i < fieldCount; ++i) {
-        if (row[i] != null && row[i].getClass().getName().equals("org.bson.types.ObjectId")) {
-          columns.put(fieldNames.get(i), String.valueOf(row[i]));
-        } else {
-          columns.put(fieldNames.get(i), row[i]);
-        }
+        columns.put(fieldNames.get(i), row[i]);
       }
       rows.add(columns);
     }
